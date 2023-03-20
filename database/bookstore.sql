81a7852e3901bd6325313275f10a04a70556c1d0
PhamDat <phamdat999666@gmail.com>
2023-03-20 15:37:06 +0700

    Sửa lại các hàm cho phân quyền (Permission) - User: status, ajaxGroup, ajaxStatus - Mở phân quyền Group

--- database/bookstore.sql
+++ database/bookstore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 16, 2023 at 03:31 PM
+-- Generation Time: Mar 20, 2023 at 09:35 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -45,10 +45,10 @@ CREATE TABLE `group` (
 --
 
 INSERT INTO `group` (`id`, `name`, `group_acp`, `created`, `created_by`, `modified`, `modified_by`, `status`, `ordering`, `privilege_id`) VALUES
-(1, 'Admin', 1, '2022-10-18', 'admin', '2023-01-11', 'admin', '1', '11', '1,2,3,4,5,6,7,8,9,10'),
+(1, 'Admin', 1, '2022-10-18', 'admin', '2023-01-11', 'admin', '1', '11', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17'),
 (2, 'Manager', 1, '2022-10-05', 'admin', '2022-10-13', 'nguyenvana', '1', '10', '1,2,3,4,6,7,8,9,10'),
-(3, 'Member', 0, '2022-10-08', NULL, '2023-01-11', 'admin', '1', '11', ''),
-(4, 'Register', 0, '2022-10-08', NULL, '2023-03-11', 'admin', '0', '10', '');
+(3, 'Member', 0, '2022-10-08', NULL, '2023-01-11', 'admin', '1', '11', '1'),
+(4, 'Register', 0, '2022-10-08', NULL, '2023-03-11', 'admin', '0', '13', '');
 
 -- --------------------------------------------------------
 
@@ -72,13 +72,20 @@ INSERT INTO `privilege` (`id`, `name`, `module`, `controller`, `action`) VALUES
 (1, 'Hiển thị danh sách người dùng', 'backend', 'user', 'list'),
 (2, 'Thay đổi status của người dùng', 'backend', 'user', 'status'),
 (3, 'Cập nhật thông tin người dùng', 'backend', 'user', 'form'),
-(4, 'Thay đổi status của người dùng sử dụng Ajax', 'backend', 'user', 'ajaxStatus'),
-(5, 'Xoá một hoặc nhiều người dùng', 'backend', 'user', 'trash'),
+(4, 'Thay đổi status của người dùng sử dụng Ajax', 'backend', 'user', 'ajaxUserStatus'),
+(5, 'Xoá một hoặc nhiều người dùng', 'backend', 'user', 'delete'),
 (6, 'Thay đổi vị trí hiển thị của các người dùng', 'backend', 'user', 'ordering'),
 (7, 'Truy cập menu Admin Control Panel', 'backend', 'index', 'index'),
 (8, 'Đăng nhập Admin Control Panel', 'backend', 'index', 'login'),
 (9, 'Đăng xuất Admin Control Panel', 'backend', 'index', 'logout'),
-(10, 'Cập nhật thông tin tài khoảng quản trị', 'backend', 'index', 'profile');
+(10, 'Cập nhật thông tin tài khoảng quản trị', 'backend', 'index', 'profile'),
+(11, 'Xoá một hoặc nhiều người dùng với Bulk', 'backend', 'user', 'deleteMult'),
+(12, 'Chọn Group cho User thông qua selectBox', 'backend', 'user', 'selectGroupForUser'),
+(13, 'Hiển thị danh sách group', 'backend', 'group', 'list'),
+(14, 'Cập nhật thông tin group', 'backend', 'group', 'form'),
+(15, 'Xoá một group', 'backend', 'group', 'delete'),
+(16, 'Thay đổi status của group sử dụng Ajax', 'backend', 'group', 'ajaxStatus'),
+(17, 'Thay đổi groupACP của group sử dụng Ajax', 'backend', 'group', 'ajaxGroupACP');
 
 -- --------------------------------------------------------
 
@@ -110,14 +117,14 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `username`, `email`, `fullname`, `password`, `created`, `created_by`, `modified`, `modified_by`, `register_date`, `register_ip`, `status`, `ordering`, `group_id`) VALUES
 (1, 'admin', 'admin@gmail.com', 'admin', '12345', '2022-12-28 00:00:00', '1', '2023-03-11 08:37:20', '1', '2023-02-25 14:39:22', NULL, 1, 10, 1),
 (2, 'manager', 'manager@gmail.com', 'Manager', '12345', '2022-12-28 02:55:00', '5', '2023-03-09 03:39:47', '5', '2023-02-25 14:39:22', NULL, 0, 10, 2),
-(3, 'member', 'member@gmail.com', 'Member', '12345', '2022-12-28 08:55:00', '1', '2023-03-09 08:46:07', '1', '2023-02-25 14:39:22', NULL, 1, 10, 3),
+(3, 'member', 'member@gmail.com', 'Member', '12345', '2022-12-28 08:55:00', '1', '2023-03-09 08:46:07', '1', '2023-02-25 14:39:22', NULL, 0, 10, 3),
 (4, 'register', 'register@gmail.com', 'Register', '12345', '2022-12-28 10:04:00', '5', '2023-03-11 08:57:58', '5', '2023-02-25 14:39:22', NULL, 1, 10, 4),
-(5, 'nguyenvana', 'nguyenvana@gmail.com', 'Nguyen Van A123', '12345', '2023-02-07 07:42:54', '1', '2023-03-11 08:52:17', '5', '2023-02-25 14:39:22', NULL, 1, 10, 1),
+(5, 'nguyenvana', 'nguyenvana@gmail.com', 'Nguyen Van A123', '12345', '2023-02-07 07:42:54', '1', '2023-03-11 08:52:17', '5', '2023-02-25 14:39:22', NULL, 1, 10, 2),
 (35, 'nguyenvanb', 'nguyenvanb@gmail.com', 'Nguyen Van B', '12345', '2023-02-25 22:33:16', NULL, '2023-03-11 09:39:04', '1', '2023-02-25 16:02:16', '::1', 1, 10, 4),
 (36, 'nguyenvanaa', 'nguyenvanaa@gmail.com', 'Nguyen Van A', '12345', '2023-02-25 22:38:31', NULL, '2023-02-25 22:38:31', NULL, '2023-02-25 16:02:31', '::1', 0, 10, 0),
-(37, 'nguyenvana1', 'nguyenvana1@gmail.com', 'Nguyen Van A1', '12345', '2023-02-25 22:44:13', NULL, '2023-02-25 22:44:13', NULL, '2023-02-25 16:02:13', '::1', 0, 10, 0),
-(38, 'admin123123123', 'phamdat9966@gmail.com', 'PhamDat22222', 'dasdsadas', '2023-02-25 23:34:00', NULL, '2023-02-25 23:34:00', NULL, '2023-02-25 17:02:00', '::1', 0, 10, 0),
-(39, 'admin1231231236666', 'phamdat9966666@gmail.com', 'Nguyen Van A', 'dasdsadsad', '2023-02-25 23:55:05', NULL, '2023-03-11 09:40:10', '1', '2023-02-25 17:02:05', '::1', 0, 10, 4);
+(41, 'fouder01', 'phamdat9966@gmail.com', 'admin123', '123456', '2023-03-18 08:12:00', '2', '2023-03-18 14:12:00', NULL, '2023-03-18 14:12:00', NULL, 0, 10, 4),
+(42, 'fouder02', 'phamdat9966@gmail.com', 'admin123', '123456', '2023-03-18 08:12:00', '2', '2023-03-18 14:12:00', NULL, '2023-03-18 14:12:00', NULL, 0, 10, 4),
+(43, 'fouder03', 'phamdat9966@gmail.com', 'admin123', '123456', '2023-03-18 08:12:00', '2', '2023-03-18 14:12:00', NULL, '2023-03-18 14:12:00', NULL, 0, 10, 4);
 
 --
 -- Indexes for dumped tables
@@ -149,19 +156,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `group`
 --
 ALTER TABLE `group`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
 
 --
 -- AUTO_INCREMENT for table `privilege`
 --
 ALTER TABLE `privilege`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=172;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
