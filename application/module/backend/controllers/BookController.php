75e97cdd3a57837abc92683fe092bf1378335f88
ADMIN\Admin <phamdat999666@gmail.com>
2023-04-21 14:54:55 +0700

    Revert "b24-fix lỗi: GD extension missing. Vào Vào php.ini file tìm cụm ;extension=gd xóa ; rồi restart lại Apachi. Sẽ vào được images bình thường"
    This reverts commit ee6f7df5f8a98e8d57c7e072937ffb8346db618d.

--- application/module/backend/controllers/BookController.php
+++ application/module/backend/controllers/BookController.php
@@ -181,41 +181,11 @@ class BookController extends Controller
         Session::set('search', '');
         Session::set('status', '');
     }
-    
-    public function gdfixAction()
-    {
-        
-        if (version_compare(PHP_VERSION, '5.6.0') >= 0) {
-            echo ' [OK] PHP version is newer than 5.6: '.phpversion();
-        } else {
-            echo ' [ERROR] Your PHP version is too old for CKFinder 3.x.';
-        }
-        
-        if (!function_exists('gd_info')) {
-            echo ' [ERROR] GD extension is NOT enabled.';
-        } else {
-            echo ' [OK] GD extension is enabled.';
-        }
-        
-        if (!function_exists('finfo_file')) {
-            echo ' [ERROR] Fileinfo extension is NOT enabled.';
-        } else {
-            echo ' [OK] Fileinfo extension is enabled.';
-        }
-        
-        /*
-         * Nếu có báo "[ERROR] GD extension is NOT enabled"
-         *  
-         *  Vào php.ini file
-            tìm cụm ;extension=gd
-            xóa ; rồi restart lại Apachi
-         */
-    }
-    
+
     // ACTION : ADD & EDIT
     public function formAction($option = null)
     {
-    
+        
 //         echo "<pre>book Controller";
 //         print_r($this->_arrParam);
 //         echo "</pre>";
