79634b2ea3bc6bbd96088933f620b0575ce25750
ADMIN\Admin <phamdat999666@gmail.com>
2023-05-09 11:00:37 +0700

    b24-frontend-banner

--- application/module/frontend/views/index/index.php
+++ application/module/frontend/views/index/index.php
@@ -1,6 +1,6 @@
 <?php 
 
-require_once 'index_homeSlider.php';
+require_once 'index_banner.php';
 require_once 'index_bookProduct.php';
 require_once 'index_serviceLayout.php';
 require_once 'index_categoryProduct.php';
---
+++ application/module/frontend/views/index/index_banner.php
@@ -0,0 +1,25 @@
+<?php
+$arrBanner          = array('slider1.jpg','slider2.jpg','slider3.jpg','slider4.jpg','slider5.jpg');
+
+$xhtmlBanner    = '';           
+foreach ($arrBanner as $imgBanner){
+    $urlBanner         = UPLOAD_URL . 'banner' . DS . $imgBanner;
+    $strSpecial1       = '\\';
+    $strSpecial2       = "/";
+    $urlBanner         = str_replace($strSpecial1 ,$strSpecial2, $urlBanner);
+    $xhtmlBanner      .= '<div>
+                			<a href="" class="home text-center"> <img
+                				src="'.$urlBanner.'" alt=""
+                				class="bg-img blur-up lazyload">
+                			</a>
+                		</div>';
+}
+
+?>
+<section class="p-0 my-home-slider">
+	<div class="slide-1 home-slider">
+		<?php 
+		  echo $xhtmlBanner;
+		?>
+	</div>
+</section>
